68dba335ffd7a3bed6e2f9a45544b9b4389c2eec
Gui12x9 <guilhermejesus568@gmail.com>
2024-03-06 19:15:14 -0300

    alterações gerais

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const masc = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao() {
+    form.style.transition =" 2s left ease-in-out"
+     form.style.left = "50%"
+     form.style.transform ="translateX(-50%)"
+     masc.style.visibility ="visible"
+}
 
 
+function cliqueiNaMascara(){
+
+    form.style.transition ="2s  ease-in-out"
+form.style.left ="-300px"
+form.style.transform="translateX(0%)"
+    masc.style.visibility = "hidden"
+    masc.style.transition= "2s ease-in-out"
+}
